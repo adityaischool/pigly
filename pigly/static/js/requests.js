d4579d734f25322dffe8ca89266b8a374b312d38
SindhujaJeyabal <sindhuja.sj@gmail.com>
2015-03-08 06:43:42 -0700

    updated html files linked via viewhandler

--- pigly/static/js/requests.js
+++ pigly/static/js/requests.js
@@ -1,15 +1,14 @@
 //***************** ALL BACK BUTTONS ******************//
-
-$('#fin_overview_back').click(function()
-{
-	$('#fin_overview').hide();
-	$('#login').show();
-});
+function separate_comma(ip_str){
+	var ret_str = ip_str.split('').reverse().join('');
+	ret_str = ret_str.replace(/(.{3})/g,",$1").split('').reverse().join('');
+	ret_str = ret_str.replace(/,$/, "");
+	return ret_str;
+}
 
 
 //****************** LOGIN PAGE ***********************//
 jQuery(document).ready(function($) {
-	$('#fin_overview').hide();
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "https://api.levelmoney.com/api/v2/hackathon/login", true);
 	xhr.setRequestHeader('Content-Type', 'application/json');
@@ -17,33 +16,21 @@ jQuery(document).ready(function($) {
 	xhr.onloadend = function() {
 	    var parsed = JSON.parse(this.response);
 	    var pretty = JSON.stringify(parsed, null, 2);
-	    document.getElementById('login_caption').textContent = "WELCOME!";
 	};
 	xhr.onerror = function(err) {
-	    document.getElementById('login_caption').textContent = "ugh an error. i can't handle this right now.";
+	    // document.getElementById('login_caption').textContent = "ugh an error. i can't handle this right now.";
 	};
 	args = {"email": "hackdoc@levelmoney.com", "password": "hackathon1"};
 	xhr.send(JSON.stringify(args));
 	// alert("1234567890".replace(/(.{3})/g,",$1"));
-});
 
 
-//****************** FIN OVERVIEW ***********************//
-function separate_comma(ip_str){
-	var ret_str = ip_str.split('').reverse().join('');
-	ret_str = ret_str.replace(/(.{3})/g,",$1").split('').reverse().join('');
-	ret_str = ret_str.replace(/,$/, "");
-	return ret_str;
-}
-$("#login_next").click(function()
-{
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "https://api.levelmoney.com/api/v2/hackathon/get-accounts", true);
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.setRequestHeader('Accept', 'application/json');
 	xhr.onloadend = function() {
 		var accounts = JSON.parse(this.response)['accounts'];
-		$('#login').hide();
 		var total_balance = 0;
 		$('#fin_overview_accounts').empty();
 		for(i=0; i < accounts.length; i++){
@@ -63,133 +50,7 @@ $("#login_next").click(function()
 	};
 	args = {"args": {"uid":  1110568334, "token":  "209E16DD45691753346973B767432F93", "api-token":  "HackathonApiToken"}};
 	xhr.send(JSON.stringify(args));
-	$('#fin_overview').show();
 });
-
-//****************** AGE ***********************//
-$("#get_age").click(function()
-{
-	$('#login').hide();
-	$("#age").show();
-	$('.demo-btn').hover(function() {
-          $(this).addClass("animated pulse");
-        }, function() {
-          $(this).removeClass("animated pulse");
-        });
-      
-        $('.demo-btn').click(function() {
-          if ($(this).find(".goal-img").hasClass("add-btn-checked") || $(this).hasClass("custom-btn")){
-            $(this).removeClass("btn-checked");
-            $(this).find(".goal-img").removeClass("add-btn-checked")
-          } else {
-            $(this).addClass("btn-checked");
-            $(this).find(".goal-img").addClass("add-btn-checked");
-          }
-        });
-      
-        $('.cc-loan-btn').click(function(){
-          console.log("loan or cc btn clicked");
-          $(".cc-loan-btn").addClass("demo-btn-checked");
-        });
-      
-        $('.custom-btn').click(function(){
-          console.log("custom button");
-          $(".goals-questions").removeClass("show").addClass("hide");
-          $(".custom-goal").addClass("animated fadeInRight show");
-        });
-      
-        $('.goal-back').click(function(){
-          console.log("goal back button click");
-          $(".goals-questions").addClass("animated fadeInLeft show");
-          $(".custom-goal").removeClass("show").addClass("hide");
-        });
-});
-
-//****************** GENDER ***********************//
-$("#age_next").click(function()
-{
-	$("#age").hide();
-	$('#gender').show();
-
-    $('.gender-icon').hover(function() {
-      $('.gender-icon').innerHTML("eh");
-    });
-  
-  
-    $('.demo-btn').hover(function() {
-      $(this).addClass("animated pulse");
-    }, function() {
-      $(this).removeClass("animated pulse");
-    });
-  
-    $('.demo-btn').click(function() {
-      if ($(this).find(".goal-img").hasClass("add-btn-checked") || $(this).hasClass("custom-btn")){
-        $(this).removeClass("btn-checked");
-        $(this).find(".goal-img").removeClass("add-btn-checked")
-      } else {
-        $(this).addClass("btn-checked");
-        $(this).find(".goal-img").addClass("add-btn-checked");
-      }
-    });
-  
-    $('.cc-loan-btn').click(function(){
-      console.log("loan or cc btn clicked");
-      $(".cc-loan-btn").addClass("demo-btn-checked");
-    });
-  
-    $('.custom-btn').click(function(){
-      console.log("custom button");
-      $(".goals-questions").removeClass("show").addClass("hide");
-      $(".custom-goal").addClass("animated fadeInRight show");
-    });
-  
-    $('.goal-back').click(function(){
-      console.log("goal back button click");
-      $(".goals-questions").addClass("animated fadeInLeft show");
-      $(".custom-goal").removeClass("show").addClass("hide");
-    });
-});
-
-//****************** GOALS ***********************//
-$("#gender_next").click(function()
-{
-	$('#gender').hide();
-	$('#goals').show();
-
-	$('.demo-btn').hover(function() {
-        $(this).addClass("animated pulse");
-      }, function() {
-        $(this).removeClass("animated pulse");
-      });
-    
-      $('.demo-btn').click(function() {
-        if ($(this).find(".goal-img").hasClass("add-btn-checked") || $(this).hasClass("custom-btn")){
-          $(this).removeClass("btn-checked");
-          $(this).find(".goal-img").removeClass("add-btn-checked")
-        } else {
-          $(this).addClass("btn-checked");
-          $(this).find(".goal-img").addClass("add-btn-checked");
-        }
-      });
-    
-      $('.cc-loan-btn').click(function(){
-        console.log("loan or cc btn clicked");
-        $(".cc-loan-btn").addClass("demo-btn-checked");
-      });
-    
-      $('.custom-btn').click(function(){
-        console.log("custom button");
-        $(".goals-questions").removeClass("show").addClass("hide");
-        $(".custom-goal").addClass("animated fadeInRight show");
-      });
-    
-      $('.goal-back').click(function(){
-        console.log("goal back button click");
-        $(".goals-questions").addClass("animated fadeInLeft show");
-        $(".custom-goal").removeClass("show").addClass("hide");
-      });
-});
-
 
 //****************** YET TO BE USED ***********************//
 $("#get_transactions").click(function()
--- pigly/views.py
+++ pigly/views.py
@@ -9,60 +9,85 @@ import json
 
 @app.route('/', methods=['GET', 'POST'])
 def land():
-	return render_template("main.html")
+	return render_template("login.html")
 
-#AJAX - analytics dashboard 
-@app.route('/_getAnalytics', methods=['GET', 'POST'])
-def _getAnalytics():
-	print "Inside analytics"
-	time = json.loads(request.args.get('time'))
-	timeperiod = "DAY"
-	print "TIME VALUE =",time
-	print timeperiod
-	driverID = "default@default.com"
-	val = travelManager.GetRides(driverID,time,timeperiod)
-	return Response(json.dumps(val),mimetype='application/json')
+@app.route('/age', methods=['GET', 'POST'])
+def _age():
+	print "AGE"
+	return render_template('age.html')
 
+@app.route('/gender', methods=['GET', 'POST'])
+def _gender():
+	return render_template('gender.html')
 
-def _transactions(transaction_list):
-	transactions = json.loads(transaction_list)['transactions']
-	cat_len = int(math.floor(len(transactions)*0.25))
-	print cat_len
-	cat_list = [transactions[:cat_len], transactions[cat_len:cat_len*2], transactions[cat_len*2:cat_len*3], transactions[cat_len*3:]]
-	amount_list = list()
-	catname_list = ['Food and Drinks', 'Travel', 'Electronics', 'Clothes and Shoes']
-	for transactions in cat_list:
-		amount = 0
-		for trans in transactions:
-			period = trans['transaction-time'][:7]
-			if period == '2015-02' or period == '2015-03' and trans['amount'] < 0:
-				amount += trans['amount']
-		amount_list.append(-amount)
-	cat_spends = dict()
-	for i in range(len(catname_list)):
-		cat_spends[catname_list[i]] = cat_spends.get(catname_list[i],0) + amount_list[i]
-	return cat_spends
+@app.route('/zipcode', methods=['GET', 'POST'])
+def _zipcode():
+	return render_template('zipcode.html')
 
-def _accounts(account_list):
-	# print account_list
-	accounts = json.loads(account_list)['accounts']
-	ac_list = list()
-	# for ac in accounts:
-	# 	ac_list.append({'last-digits':ac['last-digits'], 
-	# 					'balance': ac['balance'],
-	# 					'account-type': ac['account-type']})
-	# print ac_list
-	for ac in accounts:
-		ac_list.append((ac['last-digits'],ac['balance']));
-	return ac_list
-
-@app.route('/_fin_overview', methods=['GET', 'POST'])
+@app.route('/fin_overview', methods=['GET', 'POST'])
 def _fin_overview():
-	accounts = request.args.get('accounts',0,type=str)
-	# print "\n\n\n",accounts
-	# acts = json.dumps(_accounts(accounts))
-	return render_template('login.html', accounts=_accounts(accounts))
-	# return acts
+	return render_template('fin_overview.html')
 
-def _projected_transactions(proj_transaction_list):
-	print proj_transaction_list
+@app.route('/goals', methods=['GET', 'POST'])
+def _goals():
+	return render_template('goals.html')
+
+@app.route('/challenges', methods=['GET', 'POST'])
+def _challenges():
+	return render_template('challenges.html')
+
+# #AJAX - analytics dashboard 
+# @app.route('/_getAnalytics', methods=['GET', 'POST'])
+# def _getAnalytics():
+# 	print "Inside analytics"
+# 	time = json.loads(request.args.get('time'))
+# 	timeperiod = "DAY"
+# 	print "TIME VALUE =",time
+# 	print timeperiod
+# 	driverID = "default@default.com"
+# 	val = travelManager.GetRides(driverID,time,timeperiod)
+# 	return Response(json.dumps(val),mimetype='application/json')
+
+
+# def _transactions(transaction_list):
+# 	transactions = json.loads(transaction_list)['transactions']
+# 	cat_len = int(math.floor(len(transactions)*0.25))
+# 	print cat_len
+# 	cat_list = [transactions[:cat_len], transactions[cat_len:cat_len*2], transactions[cat_len*2:cat_len*3], transactions[cat_len*3:]]
+# 	amount_list = list()
+# 	catname_list = ['Food and Drinks', 'Travel', 'Electronics', 'Clothes and Shoes']
+# 	for transactions in cat_list:
+# 		amount = 0
+# 		for trans in transactions:
+# 			period = trans['transaction-time'][:7]
+# 			if period == '2015-02' or period == '2015-03' and trans['amount'] < 0:
+# 				amount += trans['amount']
+# 		amount_list.append(-amount)
+# 	cat_spends = dict()
+# 	for i in range(len(catname_list)):
+# 		cat_spends[catname_list[i]] = cat_spends.get(catname_list[i],0) + amount_list[i]
+# 	return cat_spends
+
+# def _accounts(account_list):
+# 	# print account_list
+# 	accounts = json.loads(account_list)['accounts']
+# 	ac_list = list()
+# 	# for ac in accounts:
+# 	# 	ac_list.append({'last-digits':ac['last-digits'], 
+# 	# 					'balance': ac['balance'],
+# 	# 					'account-type': ac['account-type']})
+# 	# print ac_list
+# 	for ac in accounts:
+# 		ac_list.append((ac['last-digits'],ac['balance']));
+# 	return ac_list
+
+# @app.route('/_fin_overview', methods=['GET', 'POST'])
+# def _fin_overview():
+# 	accounts = request.args.get('accounts',0,type=str)
+# 	# print "\n\n\n",accounts
+# 	# acts = json.dumps(_accounts(accounts))
+# 	return render_template('login.html', accounts=_accounts(accounts))
+# 	# return acts
+
+# def _projected_transactions(proj_transaction_list):
+# 	print proj_transaction_list
